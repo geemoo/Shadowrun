0924e73f35bfa5246a158499c10d03d5b1f1b98b
Steven Lusk <prodigaljedi@gmail.com>
2009-01-01 17:21:06 -0500

    Up to date with sourcebooks up to end of 2008
    Updated charSheet to include all new information.
    
    Updates gmscreen with errata fro SR4.
    
    Deleted the options directory, so found a new home for the
    options file.
    
    Fixed some sourcebook citation errors in spellTable.
    Also added page numbers for all spells.
    
    Determined metaphors for Umbra's spirits.
    This necessitated some background story additions and updates.
    
    Trying to create a custom spelltable for Umbra.
    Will eventually include personally designed spells.

--- roller.rb
+++ roller.rb
@@ -1,14 +1,35 @@
 require 'optparse'
     
 class Roller
+
+    class GlitchException < RuntimeError
+        attr :critical
+
+        def initialize( critical )
+            @critical = critical
+        end
+
+        def to_s()
+            if( critical )
+                return "CRITICAL GLITCH!!!"
+            else
+                return "Glitch!"
+            end
+        end
+    end
+    
     def initialize()
         edge = false
         extended = false
+        threshold = 0
         
         @options = OptionParser.new()
 
         @options.on("--edge", "-e") {|val| edge = true }
-        @options.on("--extended", "-x") {|val| extended = true }
+        @options.on("--extended=THRESHOLD", "-x") do |val| 
+            extended = true
+            threshold = val.to_i()
+        end
         @options.on("--help", "-h") {|val| puts @options.to_s(); exit; }
 
         @dice = @options.parse(ARGV)[0].to_i()
@@ -16,14 +37,14 @@ class Roller
         if(edge)
             edge()
         elsif(extended)
-            extended()
+            extended(threshold)
         else
             standard()
         end
     end
 
     # rolls random dice
-    # return an array of [ fives, sixes, ones ] rolled
+    # return an array of [ fives, sixes ] rolled
     def roll(dice)
         fives = 0
         sixes = 0
@@ -39,43 +60,79 @@ class Roller
                 ones = ones.next()
             end
         end
-        return [ fives, sixes, ones ]
-    end
 
-    def output(ones, hits)
         if( (ones * 2) >= @dice )
-            if( hits <= 0 )
-                puts("CRITICAL GLITCH!!!")
+            if( (fives + sixes) <= 0 )
+                raise GlitchException.new(true) 
             else
-                puts("Glitch!")
+                raise GlitchException.new(false) 
             end
-        else
-            puts(hits)
         end
+        return [ fives, sixes ]
     end
     
     def standard()
-        result = roll( @dice )
-        output( result[2], result[0] + result[1] )
+        begin
+            result = roll( @dice )
+            hits = result[0] + result[1]
+            puts( "Hits:  #{hits}" )
+            return hits
+        rescue GlitchException
+            puts($!.to_s())
+        end
     end
     
     def edge()
-        result = roll( @dice )
-        hits = 0
-        ones = 0
-
-        until( result[1] <= 0 )
-            hits += result[0] + result[1]
-            ones += result[2]
-            result = roll( result[1] )
+        begin
+            result = roll( @dice )
+        rescue GlitchException
+            puts($!.to_s())
+            return 0
         end
 
-        output(ones, hits)
+        hits = result[0] + result[1]
+
+        begin
+            until( result[1] <= 0 )
+                result = roll( result[1] )
+                hits += result[0] + result[1]
+            end
+        rescue GlitchException
+            retry
+        end
+
+        puts( "Hits:  #{hits}" )
+        return hits
     end
 
-    def extended()
+    def extended(threshold)
+        total_hits = 0
+        rolls = 0
         
+        begin
+            while( total_hits < threshold )
+                rolls += 1
+                result = roll( @dice )
+                total_hits += result[0] + result[1]
+            end
+            output = "Intervals:  #{rolls}"
+
+        rescue GlitchException
+            if($!.critical)
+                output = "#{$!.to_s()}   Intervals:  #{rolls}  Hits:  #{total_hits}"
+            else
+                total_hits -= ( rand(6) + 1 )
+                retry
+            end
+        end
+
+        if( rolls <= @dice * @dice )
+            puts( output )
+        else
+            puts( "#{output}!" )
+        end
     end
     
-    roller = Roller.new()
 end # Roller
+
+roller = Roller.new()
